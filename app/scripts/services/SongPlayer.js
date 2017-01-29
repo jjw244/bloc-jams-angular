0fd4b2a117860dfe1ce3ccdb760b0c5a82bd7dbf
Jennifer washburn <jjw244@utexas.edu>
2017-01-29 14:00:05 -0600

    Angular Checkpoint 7 - Assignment - Complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,7 +1,15 @@
 (function() {
     function SongPlayer() {
+        /**
+        * @desc Song Player object housing state of song item (pause/play/stop)
+        * @type {Object}
+        */
         var SongPlayer = {};
 
+        /**
+        * @desc Song object state of current song
+        * @type {Object}
+        */
         var currentSong = null;
         
         /**
@@ -28,12 +36,26 @@
 
             currentSong = song;
         };
+        
+        /**
+         * @function playSong
+         * @desc Plays currently clicked song and loads new audio file as currentBuzzObject
+         * @param {Object} song
+         */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        }
 
+        /**
+         * @function SongPlayer.play
+         * @desc Checks to see if the currentSong is the clicked song.  If the currentSong is different, then the currentSong is stopped and changed to the clicked song. If the currentSong is the same song clicked, then the currentSong is checked to see if it is paused and plays the song if it is.
+         * @param {Object} song
+         */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
                 
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
@@ -42,12 +64,17 @@
             }    
         };
         
+        /**
+         * @function SongPlayer.pause
+         * @desc If the currentSong is playing and the play/pause button is clicked, the song is paused and song.playing is set to false.
+         * @param {Object} song
+         */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
         };
         
-          return SongPlayer;
+        return SongPlayer;
      }
  
      angular
